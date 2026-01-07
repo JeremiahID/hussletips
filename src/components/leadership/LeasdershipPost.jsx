5a9e569990e19bef10b11fc5b897ad28e1bacd1a
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-07 10:42:33 +0100

    tweaking

--- src/components/leadership/LeasdershipPost.jsx
+++ src/components/leadership/LeasdershipPost.jsx
@@ -1,9 +1,9 @@
 
 import LeadershipPostCard from "./LeadershipPostCard";
 import leadership_banner from '../../images/leadership-banner.webp'
-const LeadershipPosts = ({ posts, link, header }) => {
+const LeadershipPosts = ({ posts}) => {
   return (
-    <section className="container   py-5 ">
+    <section className="container py-5 ">
       <div className="mb-4">
         <div className=" bg-info d-flex" style={{ height: "150px" }}>
          <img src={leadership_banner} alt="leadership-banner" srcset=""  className=" img-fluid leadership-banner-img col-4"/>
@@ -11,12 +11,12 @@ const LeadershipPosts = ({ posts, link, header }) => {
       </div>
       <div className="row g-4">
         {posts.map(post => (
-          <div key={post.id} className="col-md-4">
+          <div key={post.id} className="col-md-3">
             <LeadershipPostCard post={post} />
           </div>
         ))}
         {posts.map(post => (
-          <div key={post.id} className="col-md-4">
+          <div key={post.id} className="col-md-3">
             <LeadershipPostCard post={post} />
           </div>
         ))}
