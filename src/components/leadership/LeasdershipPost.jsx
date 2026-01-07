e0a0114f9e5e3d76e75456ba9b069685048f8a14
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-07 11:25:45 +0100

    images testing dor leadership section banner

--- src/components/leadership/LeasdershipPost.jsx
+++ src/components/leadership/LeasdershipPost.jsx
@@ -1,12 +1,18 @@
 
 import LeadershipPostCard from "./LeadershipPostCard";
-import leadership_banner from '../../images/leadership-banner.webp'
+import leadership_banner_one from '../../images/leadership-banner.webp'
+// import leadership_banner_four from '../../images/leadership-banner-IV.webp'
+
 const LeadershipPosts = ({ posts}) => {
   return (
     <section className="container py-5 ">
       <div className="mb-4">
-        <div className=" bg-info d-flex" style={{ height: "150px" }}>
-         <img src={leadership_banner} alt="leadership-banner" srcset=""  className=" img-fluid leadership-banner-img col-4"/>
+        <div className=" banner-wrapper d-flex justify-content-between" style={{ height: "150px" }}>
+         <img src={leadership_banner_one} alt="leadership-banner" srcset=""  className=" leadership-banner-img "/>
+         <button className=" btn-leadership btn text-white fw-semibold align">
+          Read More
+         </button>
+
         </div>
       </div>
       <div className="row g-4">
