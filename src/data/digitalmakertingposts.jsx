53f12cab7284c35c460bf326f41348d337e5de14
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-07 14:18:51 +0100

    code refactor

---
+++ src/data/digitalmakertingposts.jsx
@@ -0,0 +1,28 @@
+const DigitalMakertingBlogPosts= [
+  {
+    id: 1,
+    title: "The Future of Web Development",
+    image: "https://picsum.photos/600/400?random=1",
+    author: "Jeremiah Idahosa",
+    date: "Jan 3, 2026",
+    category: "Entreprenuer"
+  },
+  {
+    id: 2,
+    title: "React Performance Optimization Tips",
+    image: "https://picsum.photos/600/400?random=2",
+    author: "Jane Doe",
+    date: "Jan 1, 2026",
+    category: "Entreprenuer"
+  },
+  {
+    id: 3,
+    title: "UI/UX Principles Every Developer Should Know",
+    image: "https://picsum.photos/600/400?random=3",
+    author: "John Smith",
+    date: "Dec 28, 2025",
+    category: "Entreprenuer"
+  },
+];
+
+export default DigitalMakertingBlogPosts;
