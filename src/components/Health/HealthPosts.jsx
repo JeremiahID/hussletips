7dcdde1d13b3a1a06175131ca31fbd3174086856
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-11 13:15:38 +0100

    New section

---
+++ src/components/Health/HealthPosts.jsx
@@ -0,0 +1,16 @@
+const HealthPosts = () => {
+    return ( 
+        <div className="container py-5">
+            <div className="row">
+            <div className="col-md-8">
+
+            </div>
+            <div className="col-md-4">
+
+            </div>
+            </div>
+      </div>
+     );
+}
+ 
+export default HealthPosts;
