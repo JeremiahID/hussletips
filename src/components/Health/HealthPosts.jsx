46bc08961203faa4e41f3d625124ff675c35f35c
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-11 13:21:57 +0100

    scripting

--- src/components/Health/HealthPosts.jsx
+++ src/components/Health/HealthPosts.jsx
@@ -1,9 +1,13 @@
-const HealthPosts = () => {
+import HealthPostCard from "./HealthPostCard";
+
+const HealthPosts = ({ posts }) => {
     return ( 
         <div className="container py-5">
             <div className="row">
             <div className="col-md-8">
-
+                {posts.map(post => (
+                    <HealthPostCard key ={post.id} post={post} />
+                ))}
             </div>
             <div className="col-md-4">
 
