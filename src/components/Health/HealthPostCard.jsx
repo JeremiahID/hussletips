34297763bb5018e80a29901557f2f31049c8f515
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-12 12:02:19 +0100

    minor chnages

--- src/components/Health/HealthPostCard.jsx
+++ src/components/Health/HealthPostCard.jsx
@@ -1,9 +1,9 @@
 const HealthPostCard = ({ post }) => {
   return (
-    <div className="d-flex bg-info mt-2">
-      <div className="bg-secondary rounded me-3 health-post-card-img"></div>
+    <div className="mt-4 card flex-row shadow-sm border-0">
+      <div className="bg-secondary me-3 health-post-card-img"></div>
 
-      <div className="d-flex flex-column align-items-start justify-content-evenly bg-dark w-50">
+      <div className="d-flex flex-column align-items-start justify-content-between w-50">
         <small className="text-primary fw-semibold">{post.category}</small>
         <h6 className="fw-bold mb-1">{post.title}</h6>
         <p className=" small mb-1">{post.excerpt}</p>
--- src/components/Health/HealthPosts.jsx
+++ src/components/Health/HealthPosts.jsx
@@ -2,14 +2,14 @@ import HealthPostCard from "./HealthPostCard";
 
 const HealthPosts = ({ posts }) => {
     return ( 
-        <div className="container py-3 bg-warning ">
-            <div className="row">
-                <div className="col-9">
+        <div className="container py-3 ">
+            <div className="row d-flex ">
+                <div className="col-md-9 col-12">
                     {posts.map(post => (
                         <HealthPostCard key ={post.id} post={post} />
                     ))}
                 </div>
-                <div className="col-md-3 bg-primary mt-2" >
+                <div className="col-md-3 col-12 bg-primary mt-2" >
 
                 </div>
             </div>
