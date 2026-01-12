6f0c371f516231fc1082ef34edbd95e65304f304
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-12 12:23:57 +0100

    test

--- src/components/Health/HealthPosts.jsx
+++ src/components/Health/HealthPosts.jsx
@@ -2,14 +2,14 @@ import HealthPostCard from "./HealthPostCard";
 
 const HealthPosts = ({ posts }) => {
     return ( 
-        <div className="container py-3 bg-info ">
+        <div className="container py-3 ">
             <div className="row d-flex ">
                 <div className="col-md-9 col-12">
                     {posts.map(post => (
                         <HealthPostCard key ={post.id} post={post} />
                     ))}
                 </div>
-                <div className="card col-md-3 col-12 shadow border-0 mt-2 aside-post-card " >
+                <div className="card col-md-3 col-12 shadow border-0 mt-4 aside-post-card " >
 
                 </div>
             </div>
