8cd3f041712d93b94888bd72897c28b25618d167
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-13 15:51:48 +0100

    aside blog post

---
+++ src/data/asidehealthblogposts.jsx
@@ -0,0 +1,13 @@
+const AsideHealthblogposts = [
+  {
+    id: 1,
+    title: "The Future of Web Development",
+    excerpt: "Exploring modern tools, frameworks.",
+    image: "https://picsum.photos/600/400?random=1",
+    author: "Jeremiah Idahosa",
+    date: "Jan 3, 2026",
+    category: "Business"
+  }
+];
+
+export default AsideHealthblogposts;
