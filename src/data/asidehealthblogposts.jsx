e65beddd5492fd3adf354984d0602fd0c3345310
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-14 15:13:47 +0100

    blog post correction

--- src/data/asidehealthblogposts.jsx
+++ src/data/asidehealthblogposts.jsx
@@ -6,7 +6,7 @@ const AsideHealthblogposts = [
     image: "https://picsum.photos/600/400?random=1",
     author: "Jeremiah Idahosa",
     date: "Jan 3, 2026",
-    category: "Business"
+    category: "Health"
   }
 ];
 
