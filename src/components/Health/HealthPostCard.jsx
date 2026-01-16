1a1f83c07f91a0c6b00367073b375a29d529384f
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-17 00:56:19 +0100

    image link change

--- src/components/Health/HealthPostCard.jsx
+++ src/components/Health/HealthPostCard.jsx
@@ -1,7 +1,14 @@
 const HealthPostCard = ({ post }) => {
   return (
     <div className="mt-4 card flex-row shadow-sm border-0">
-      <div className="bg-secondary me-3 health-post-card-img"></div>
+      <div className="bg-secondary me-3 health-post-card-img">
+        <img
+            src= {post.image}
+            alt="Hero"
+            className="w-100 h-100"
+            style={{ objectFit: 'cover' }}
+        />
+      </div>
 
       <div className="d-flex flex-column align-items-start justify-content-between w-50">
         <small className="text-primary fw-semibold">{post.category}</small>
