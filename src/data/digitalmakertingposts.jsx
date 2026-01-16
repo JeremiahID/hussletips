76cf88f84cd920cda5b44b8ada6ed787ae768dac
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-17 00:56:38 +0100

    image link change

--- src/data/digitalmakertingposts.jsx
+++ src/data/digitalmakertingposts.jsx
@@ -2,7 +2,7 @@ const DigitalMakertingBlogPosts= [
   {
     id: 1,
     title: "The Future of Web Development",
-    image: "https://picsum.photos/600/400?random=1",
+    image: "https://picsum.photos/600/400?random=12",
     author: "Jeremiah Idahosa",
     date: "Jan 3, 2026",
     category: "Digital Marketing"
@@ -10,7 +10,7 @@ const DigitalMakertingBlogPosts= [
   {
     id: 2,
     title: "React Performance Optimization Tips",
-    image: "https://picsum.photos/600/400?random=2",
+    image: "https://picsum.photos/600/400?random=6",
     author: "Jane Doe",
     date: "Jan 1, 2026",
     category: "Digital Marketing"
@@ -18,7 +18,7 @@ const DigitalMakertingBlogPosts= [
   {
     id: 3,
     title: "UI/UX Principles ",
-    image: "https://picsum.photos/600/400?random=3",
+    image: "https://picsum.photos/600/400?random=9",
     author: "John Smith",
     date: "Dec 28, 2025",
     category: "Marketing"
