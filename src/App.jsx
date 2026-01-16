d54de26047fb3adc91346854036ae63b8102f782
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-16 17:01:52 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<PostPage />} />
-        <Route path="/category/:slug" element={<CategoryPage />} />
+        {/* <Route path="/category/:slug" element={<CategoryPage />} /> */}
       </Routes>
     </Router>
   );
