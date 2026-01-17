4ed56f10b9fbd041072de6cd2c6453c660a6732f
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-17 02:19:26 +0100

    Page posts

--- src/Pages/entreprenuer/EntreprenuerPage.jsx
+++ src/Pages/entreprenuer/EntreprenuerPage.jsx
@@ -1,10 +1,32 @@
-const EntreprenuerPage = () => {
-    return ( 
-        <>
+import EntreprenuerPageCard from "./EntreprenuerPageCard";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
+const EntreprenuerPage = ({ posts, link, header }) => {
+  return (
+    <>
+        <Navbar />
+
+        <section className="container py-5 ">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+            <h2 className="fw-bold featured-post-header py-1 px-3 ">{header}</h2>
+            <a href="#" className="text-primary text-decoration-none">
+            {link}
+            </a>
+        </div>
+
+        <div className="row g-4">
+            {posts.map(post => (
+            <div key={post.id} className="col-md-4">
+                <EntreprenuerPageCard post = { post } />
+            </div>
+            ))}
+        </div>
+        </section>
         
-        </>
-     );
-}
- 
+        <Footer />
+    </>
+  );
+};
+
 export default EntreprenuerPage;
