4d5fd0689a6ec1ad71e0feafbab5149219e7504a
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-18 21:35:42 +0100

    feature post card code refactor

--- src/components/FeaturedPosts/FeaturedPosts.jsx
+++ src/components/FeaturedPosts/FeaturedPosts.jsx
@@ -1,13 +1,14 @@
 import FeaturedPostCard from "./FeaturedPostCard";
-
+import { Link } from "react-router-dom";
 const FeaturedPosts = ({ posts, link, header }) => {
   return (
     <section className="container py-5 ">
       <div className="d-flex justify-content-between align-items-center mb-4">
         <h2 className="fw-bold featured-post-header py-1 px-3 ">{header}</h2>
-        <a href="/" className="text-primary text-decoration-none">
-          {link}
-        </a>
+
+        <Link to="/entreprenuer" className="text-primary text-decoration-none">
+         {link}
+        </Link>
       </div>
 
       <div className="row g-4">
