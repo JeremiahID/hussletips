4f04d0770743c6c4057ab8772c19bf0a7957ae5b
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-19 09:54:38 +0100

    test

--- src/Pages/entreprenuer/EntreprenuerPage.jsx
+++ src/Pages/entreprenuer/EntreprenuerPage.jsx
@@ -1,4 +1,5 @@
 import EntreprenuerPageCard from "./EntreprenuerPageCard";
+import EntrepreneurImage from "../../images/Entreprenuer.png"
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
@@ -8,11 +9,9 @@ const EntreprenuerPage = ({ posts, link, header }) => {
         <Navbar />
 
         <section className="container py-5 ">
-        <div className="d-flex justify-content-between align-items-center mb-4">
-            <h2 className="fw-bold featured-post-header py-1 px-3 ">{header}</h2>
-            <a href="/"  className="text-primary text-decoration-none">
-            {link}
-            </a>
+        <div className="d-flex justify-content-center align-items-center mb-4 bg-light">
+            <img src={EntrepreneurImage} alt="Entreprenuer" />
+            <h2 className="fw-bold featured-post-header text-center fs-1 py-1 px-3 d-md-block d-none ">{header}</h2>
         </div>
 
         <div className="row g-4">
