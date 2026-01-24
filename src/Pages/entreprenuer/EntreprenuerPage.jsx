50368f9837f7e9261a2b9b03ebd9e09733c2009a
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-24 07:05:00 +0100

    update on the page

--- src/Pages/entreprenuer/EntreprenuerPage.jsx
+++ src/Pages/entreprenuer/EntreprenuerPage.jsx
@@ -10,18 +10,18 @@ const EntreprenuerPage = ({ posts, header }) => {
         <Navbar />
 
         <section className="container py-5 ">
-        <div className="d-flex justify-content-center align-items-center mb-4 ">
-            <img src={EntrepreneurImage} alt="Entreprenuer" className="d-block d-md-none" />
-            <h2 className="fw-bold featured-post-header text-center fs-1 py-1 px-3 d-md-block d-none ">{header}</h2>
-        </div>
-
-        <div className="row g-4">
-            {posts.map(post => (
-            <div key={post.id} className="col-md-4">
-                <EntreprenuerPageCard post = { post } />
+            <div className="d-flex justify-content-center align-items-center mb-4 ">
+                <img src={EntrepreneurImage} alt="Entreprenuer" className="d-block d-md-none" />
+                <h2 className="fw-bold featured-post-header text-center fs-1 py-1 px-3 d-md-block d-none ">{header}</h2>
             </div>
-            ))}
-        </div>
+
+            <div className="row g-4">
+                {posts.map(post => (
+                <div key={post.id} className="col-md-4">
+                    <EntreprenuerPageCard post = { post } />
+                </div>
+                ))}
+            </div>
         </section>
         
         <Footer />
