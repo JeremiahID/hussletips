0bba3f4658c180acc99ee74fbc081ae6078537ac
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-01-25 12:37:33 +0100

    changes to the post pages

--- src/Pages/PostPage.jsx
+++ src/Pages/PostPage.jsx
@@ -1,39 +1,39 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
+// import { useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import Navbar from "../components/Navbar/Navbar";
+// import Footer from "../components/Footer/Footer";
 
-const PostPage = () => {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
+// const PostPage = () => {
+//   const { id } = useParams();
+//   const [post, setPost] = useState(null);
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/${"digital-post/" + id}`)
-      .then(res => res.json())
-      .then(data => setPost(data))
-      .catch(err => console.error(err));
-  }, [id]);
+//   useEffect(() => {
+//     fetch(`http://localhost:8000/${"digital-post/" + id}`)
+//       .then(res => res.json())
+//       .then(data => setPost(data))
+//       .catch(err => console.error(err));
+//   }, [id]);
 
-  if (!post) return <p className="text-center py-5">Loading...</p>;
+//   if (!post) return <p className="text-center py-5">Loading...</p>;
 
-  return (
-    <>
-      < Navbar />
-      <div className="container py-5">
-        <div className="entreprenuer-post-page">
-                  <h1 className="fw-bold">{post.title}</h1>
-        <p className="text-muted">{post.category}</p>
-        <img src={post.image} alt={post.title} className="img-fluid mb-4" />
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
-        </div>
-        <div className="aside-trending">
+//   return (
+//     <>
+//       < Navbar />
+//       <div className="container py-5">
+//         <div className="entreprenuer-post-page">
+//                   <h1 className="fw-bold">{post.title}</h1>
+//         <p className="text-muted">{post.category}</p>
+//         <img src={post.image} alt={post.title} className="img-fluid mb-4" />
+//         <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//         </div>
+//         <div className="aside-trending">
 
-        </div>
-      </div>
-      <Footer />
-    </>
+//         </div>
+//       </div>
+//       <Footer />
+//     </>
 
-  );
-};
+//   );
+// };
 
-export default PostPage;
+// export default PostPage;
