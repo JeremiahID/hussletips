0c01fc47ff3fd7d43a585bee286653604df9863e
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-02-07 15:09:35 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './css/bootstrap.css'
 import './css/App.css'
 import Home from "./Pages/Home";
 import EntreprenuerPage from "./Pages/entreprenuer/EntreprenuerPage";
+import EntrepreneurPagePost from "./data/entreprenuerpagepost";
 import DigitalPostPage from "./Pages/PostPages/DigitalPostPage";
 import EntreprenuerPostPage from "./Pages/PostPages/EntreprenuerPostPage";
-import EntrepreneurPagePost from "./data/entreprenuerpagepost";
+import DigitalMarketingPage from "./Pages/DigitalMarketing/DigitalMarketingPage";
+// import DigitalMarkertingPagePost from "./data/digitalmakertingposts"
+import DM from "./data/digitalmakertingpageposts"
 import { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -24,9 +27,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/digital-post/:id" element={<DigitalPostPage />} /> 
+        <Route path="/digitalmakerting-post/:id" element={<DigitalPostPage />} /> 
         <Route path="/entreprenuer-post/:id" element={<EntreprenuerPostPage />} />
         <Route path="/entreprenuer" element={<EntreprenuerPage posts={EntrepreneurPagePost} header = {'Entreprenuer'} />} />
+        <Route path="/digitalmarketing" element={<DigitalMarketingPage posts={DM} />} />
       </Routes>
     </Router>
   );
