b88229ad38a6599b98d6a31de424f1145b0fa6b7
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2026-02-25 09:04:02 +0100

    CODE REFACTOR

--- src/Pages/entreprenuer/EntreprenuerPage.jsx
+++ src/Pages/entreprenuer/EntreprenuerPage.jsx
@@ -4,6 +4,8 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
 
+
+
 const EntreprenuerPage = ({ posts, header }) => {
   return (
     <>
